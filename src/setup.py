574c74680ca8c571f57b2fff30a1b1b1e82dd69b
scar7things <scar7things@gmail.om>
2023-07-23 08:24:53 +0300

    no errors yet

--- src/setup.py
+++ src/setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(
-    name='RFID-reader-alarm',
+    name='mypackage',
     version='0.1',
     description='A simple Python package',
     author='ahmed amin or scar7	',
     author_email='scar7things@gmail.com',
     url='https://github.com/scar7things/RFID-reader-alarm',
-    packages=['rfidalarm'],
+    packages=['mypackage'],
 )
